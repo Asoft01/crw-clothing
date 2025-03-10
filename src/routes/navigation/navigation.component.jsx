b0c14f990f6e3438ca59bef5d9a53ac64dc41f71
Adeleke Hammed Lekan <lekhad19@gmail.com>
2025-03-10 10:34:08 +0100

    Setting up firebase

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -22,6 +22,12 @@ const Navigation = () => {
             <Link className='nav-link' to='/shop'>
               SHOP
             </Link>
+            <Link className='nav-link' to='/sign-in'>
+              SIGN IN
+            </Link>
+            <Link className='nav-link' to='/sign-up'>
+              SIGN UP
+            </Link>
           </div>
         </div>
         <Outlet/>
